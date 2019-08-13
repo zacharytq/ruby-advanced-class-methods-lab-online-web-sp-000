5ab432c004145ba6ef73099927d63dbcb36eff0c
chic-page-6502 <zacharytq@gmail.com>
2019-08-13 22:07:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,10 +38,11 @@ class Song
 
   def self.find_or_create_by_name(name)
     song = self.find_by_name(name)
-    if song == false
-      song = self.create_by_name(name)
+    if song
+      song
+    else
+      self.create_by_name(name)
     end
-    song
   end
 
 
